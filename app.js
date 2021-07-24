f4bab31ee5d43840c77008955f592e7981a8f6b3
DESKTOP-N8M2BVP\s7910 <s7910565@gmail.com>
2021-07-24 21:34:16 +0800

    refactor: beautify index,new,todo page

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
 const exphbs = require('express-handlebars')
+//導入計算時間的函式
+const duration = require('./tools/duration')
+
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-//導入計算時間的函式
-const duration = require('./tools/duration')
 
 app.use((req, res, next) => {
   duration(req, res, next)
 })
 
-
+//把router全部放一起這樣比較能一次看
 app.get('/', function (req, res) {
   res.render('index')
-  // res.send('列出全部 Todo')
 })
 
-
 app.get('/new', function (req, res) {
-  // res.send('新增 Todo 頁面')
   res.render('new')
 })
 
 app.get('/:id', function (req, res) {
-  res.send('顯示一筆 Todo')
+  res.render('todo')
 })
 
 app.post('/', function (req, res) {
   res.send('新增一筆  Todo 了')
 })
+
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`)
