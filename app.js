dd5485f2e1855c7ccd10af6218625af6248d7c8a
DESKTOP-N8M2BVP\s7910 <s7910565@gmail.com>
2021-07-24 19:07:25 +0800

    feat: add the logs into logx.txt

--- app.js
+++ app.js
@@ -3,25 +3,37 @@ const app = express()
 const port = 3000
 
 const exphbs = require('express-handlebars')
-app.engine('handlebars',exphbs({defaultLayout:'main'}))
-app.set('view engine','handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
-app.get('/', (req, res) => {
+const fsLibrary = require('fs')
+
+app.use(function (req, res, next) {
+  const dateTw = new Date()
+  const log = `${dateTw.toLocaleString('zh-TW', { hour12: false })} | ${req.method} from ${req.originalUrl}\r\n`
+  fsLibrary.appendFile('./logs/logs.txt', log, (error) => {
+    if (error) throw err;
+  })
+  next()
+})
+
+
+app.get('/', function (req, res) {
   res.render('index')
   // res.send('列出全部 Todo')
 })
 
 
-app.get('/new', (req, res) => {
+app.get('/new', function (req, res) {
   // res.send('新增 Todo 頁面')
   res.render('new')
 })
- 
-app.get('/:id', (req, res) => {
+
+app.get('/:id', function (req, res) {
   res.send('顯示一筆 Todo')
 })
 
-app.post('/', (req, res) => {
+app.post('/', function (req, res) {
   res.send('新增一筆  Todo 了')
 })
 
